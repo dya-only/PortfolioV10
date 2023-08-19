bcad74b001d1f3e4d0552bbce0bcbe7cb51cd182
손보석 <dya_only@sonboseog-ui-MacBookPro.local>
2023-08-19 14:59:07 +0900

    FIX: App's folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {Fragment} from "react"
 import {Route, Routes} from "react-router-dom"
-import MainPage from "./pages/MainPage"
+import MainPage from "./Pages/MainPage"
 import Navigation from "./Components/Navigation"
 
 function App() {
